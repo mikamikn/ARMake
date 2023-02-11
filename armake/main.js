20a5a88dc13a9e740463c8d171815c29ade72cec
k_mikami <kango.m0206@gmail.com>
2023-02-11 10:32:36 +0900

    不要なファイル削除

--- armake/main.js
+++
@@ -1,9 +0,0 @@
-const video = document.getElementById('input_video');
-const canvas = document.getElementById('output_canvas');
-const ctx = canvas.getContext('2d');
-
-const config = {
-  locateFile: file => `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`
-};
-
-const faceMesh = new FaceMesh(config);
